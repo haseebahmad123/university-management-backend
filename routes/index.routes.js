49644be90dd85dd6153415fd8f20bb0d5c0ebdcc
aneesahmed330 <coolminded682@gmail.com>
2023-08-25 19:43:35 +0500

    conversion

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -7,6 +7,7 @@ require("./auth.routes")(router);
 require("./users.routes")(router);
 require("./payment.routes")(router);
 require("./order.routes")(router);
+require("./conversions.routes")(router);
 router.use(auth);
 
 require("./faqs.routes")(router);
